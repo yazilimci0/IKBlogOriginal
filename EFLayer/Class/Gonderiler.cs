581ff60c9483619b6b2121274a99449a589a86cd
yazilimci0 <yazilim8118@gmail.com>
2023-09-21 16:58:06 +0300

    düzenleme
    ...

--- EFLayer/Class/Gonderiler.cs
+++ EFLayer/Class/Gonderiler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -11,12 +12,20 @@ namespace EFLayer.Class
     {
         [Key]
         public int GonderiId { get; set; }
+        [DisplayName("Gönderi Adı")]
         public string? GonderiName { get; set; }
+        [DisplayName("Başlık")]
         public string? GonderiBaslik { get; set; }
+        [DisplayName("İçerik")]
         public string? GonderiIcerik { get; set; }
+        [DisplayName("Resimler")]
+
         public string? GonderiResim { get; set; }
+        [DisplayName("Kategori Id")]
 
         public int? kategoriId { get; set; }
+        [DisplayName("Kategori Adı")]
+
         public Kategories? Kategories { get; set; }
     }
 }
