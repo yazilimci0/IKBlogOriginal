ab82149d8e4bc5021354f4b45d5038276f25e586
yazilimci0 <yazilim8118@gmail.com>
2023-09-22 09:58:47 +0300

    yeni
    .

--- EFLayer/Class/Kategories.cs
+++ EFLayer/Class/Kategories.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,8 @@ namespace EFLayer.Class
     {
         [Key]
         public int kategoriId { get; set; }
+        [DisplayName("Kategori Adı")]
+
         public string? kategoryName { get; set; }
 
         public IEnumerable<Gonderiler>? Gonderiler { get; set; }
